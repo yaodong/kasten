dfdc4b85ffd96edc826a0579faa4ec466aa0f6ac
Yaodong Zhao <git@yaodong.org>
2020-05-23 14:10:23 -0700

    add functions

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.createNote = functions.https.onCall((data, context) => {
+  const docId = Math.floor((new Date().getTime() / 100)).toString();
+  const docRef = admin.firestore().collection('users').doc(context.auth.uid).collection('notes').doc(docId);
+
+  return new Promise((resolve, reject) => {
+    docRef.get()
+      .then(doc => {
+        if (doc.exists) {
+          return reject(new Error({ reason: 'EXISTS', message: 'Doc ref already exists' }));
+        } else {
+          docRef.set({ createdAt: admin.firestore.FieldValue.serverTimestamp() })
+          return resolve({ docId })
+        }
+      })
+      .catch(err => {
+        reject(err)
+      });
+  })
+});
